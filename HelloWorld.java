3ee966a0c5c3ba85c40116c515a3786904230f30
manonmichel <manon.michel.iphone@gmail.com>
2019-09-24 16:40:10 +0200

    second ultimate answer

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello SwEng");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+  }
+
+  public static String computeUltimateQuestion() {
+    return "still searching...";
   }
 }
